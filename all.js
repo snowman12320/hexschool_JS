e994616f6bd94ae1b8e0644e086adfc089f63945
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-10-19 16:50:51 +0800

    提問

---
+++ all.js
@@ -0,0 +1,11 @@
+// axios
+//   .get("https://hexschool.github.io/ajaxHomework/data.json")
+//   .then(function (response) {
+//     console.log(response.data);
+//     console.log(response.status);
+//     console.log(response.statusText);
+//     console.log(response.headers);
+//     console.log(response.config);
+//   });
+
+console.log(axios);

818038daa3616f4498bee92a84a7a7260d4b93cf
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-11-04 20:17:59 +0800

    微調

--- all.js
+++ all.js
@@ -11,7 +11,7 @@ function getData() {
     //迴圈出item才有值
     // console.log(base_data.上價);
     now_data = base_data;
-    renderData(base_data);
+    renderData(base_data); /*異步處理 */
   });
 }
 getData();
@@ -33,7 +33,7 @@ function renderData(showData) {
   show_list.innerHTML = str;
 }
 
-//換頁
+//換頁 (熟悉)
 const buttonGroup = document.querySelector(".button-group");
 buttonGroup.addEventListener("click", (e) => {
   //透過底下判斷式，確認點擊到的是否為 BUTTON
@@ -87,7 +87,7 @@ buttonGroup.addEventListener("click", (e) => {
 //     renderData(filterData);
 // };
 
-//搜尋
+//搜尋 (熟悉)
 let search_data = [];
 const search = document.querySelector("#search");
 search.addEventListener("click", searchData);
@@ -100,6 +100,7 @@ crop_input.addEventListener("keyup", (e) => {
 });
 function searchData(e) {
   if (e.target.nodeName === "BUTTON" || e.target.nodeName === "INPUT") {
+    /**/
     //刪除篩選的記號
     const tabs = document.querySelectorAll(".button-group button");
     tabs.forEach((item) => {
@@ -117,6 +118,7 @@ function searchData(e) {
       );
       let search_data_length = search_data.length;
       if (search_data_length === 0) {
+        /** */
         // const show_list = document.querySelector("#show_list");
         show_list.innerHTML =
           '<tr><td colspan="6" class="text-center p-3">查詢不到交易資訊QQ</td></tr>';
@@ -129,9 +131,10 @@ function searchData(e) {
   }
 }
 
-//品項排序
+//品項排序 (熟悉)
 const sort_select = document.querySelector("#sort_select");
 sort_select.addEventListener("change", (e) => {
+  /* */
   //用於選項少比對
   switch (e.target.value) {
     case "依上價排序":
@@ -162,11 +165,12 @@ function selectChange(value) {
   renderData(now_data);
 }
 
-//大小排序
+//大小排序 (認識)
 ////要選取按鈕父層再逐一比對是否為<i> 才能每個按鈕都有效果 (<thead class="js-sort-advanced">)
 const sort_btn = document.querySelector(".js-sort-advanced");
 sort_btn.addEventListener("click", (e) => {
   if (e.target.nodeName === "I") {
+    /** */
     //將點擊時取出埋藏於 i 標籤的 data-price 值
     //取點擊的選項名稱 如:上價
     let sortPrice = e.target.dataset.price;
